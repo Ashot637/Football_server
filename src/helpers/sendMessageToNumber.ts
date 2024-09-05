b6a51820f43f789746e6f0234772174cfdeb7a55
nikoghosian <vahe20006@gmail.com>
2024-09-05 11:24:46 +0400

    test feature

--- src/helpers/sendMessageToNumber.ts
+++ src/helpers/sendMessageToNumber.ts
@@ -1,15 +1,13 @@
 import axios from 'axios';
 
 export const sendMessageToNumber = async (phoneNumber: string, message: string) => {
-  const { data } = await axios.get('https://api.fortis-tele.com/api/SendSMS', {
-    params: {
-      api_id: process.env.MESSAGE_PROVIDER_API_ID,
-      api_password: `y"EnaM!Z6L`,
-      sms_type: 'T',
-      encoding: 'T',
-      sender_id: process.env.MESSAGE_PROVIDER_SENDER_ID,
-      phonenumber: phoneNumber,
-      textmessage: message,
-    },
+  const { data } = await axios.post('https://api.fortis-tele.com/api/SendSMS', {
+    api_id: process.env.MESSAGE_PROVIDER_API_ID,
+    api_password: process.env.MESSAGE_PROVIDER_API_PASSWORD,
+    sms_type: 'T',
+    encoding: 'T',
+    sender_id: process.env.MESSAGE_PROVIDER_SENDER_ID,
+    phonenumber: phoneNumber,
+    textmessage: message,
   });
 };

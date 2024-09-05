9bbcb798442608a988647e9a38902b13e9198672
nikoghosian <vahe20006@gmail.com>
2024-09-05 16:01:24 +0400

    fix 403 status code in send Message to number

--- src/helpers/sendMessageToNumber.ts
+++ src/helpers/sendMessageToNumber.ts
@@ -2,15 +2,25 @@ const axios = require('axios');
 
 export const sendMessageToNumber = async (phoneNumber: string, message: string) => {
   try {
-    const response = await axios.post('https://api.fortis-tele.com/api/SendSms', {
-      api_id: 'API203452465',
-      api_password: 'y"EnaM!Z6L',
-      sms_type: 'P',
-      encoding: 'T',
-      sender_id: 'Ballhola',
-      phonenumber: '37495444083',
-      textmessage: 'test',
-    });
+    const response = await axios.post(
+      'https://api.fortis-tele.com/api/SendSms',
+      {
+        api_id: process.env.MESSAGE_PROVIDER_API_ID,
+        api_password: process.env.MESSAGE_PROVIDER_API_PASSWORD,
+        sms_type: 'P',
+        encoding: 'T',
+        sender_id: process.env.MESSAGE_PROVIDER_SENDER_ID,
+        phonenumber: phoneNumber,
+        textmessage: message,
+      },
+      {
+        headers: {
+          'User-Agent': 'PostmanRuntime/7.41.2',
+          'Content-Type': 'application/json',
+          'Accept-Encoding': 'gzip, deflate, br',
+        },
+      },
+    );
 
     console.log('Response:', response.data);
   } catch (error: any) {

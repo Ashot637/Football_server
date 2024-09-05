5cbe52dfb865dcd20c1b0c6d74e8a3ff21f1f384
nikoghosian <vahe20006@gmail.com>
2024-09-05 11:39:06 +0400

    test feature

--- src/helpers/sendMessageToNumber.ts
+++ src/helpers/sendMessageToNumber.ts
@@ -1,31 +1,39 @@
 import axios from 'axios';
+import https from 'https';
 
 export const sendMessageToNumber = async (phoneNumber: string, message: string) => {
-  const { data } = await axios.post(
-    'https://api.fortis-tele.com/api/SendSMS',
-    {
-      api_id: process.env.MESSAGE_PROVIDER_API_ID,
-      api_password: process.env.MESSAGE_PROVIDER_API_PASSWORD,
-      sms_type: 'T',
-      encoding: 'T',
-      sender_id: process.env.MESSAGE_PROVIDER_SENDER_ID,
-      phonenumber: phoneNumber,
-      textmessage: message,
-    },
-    {
-      headers: {
-        'Postman-Token': '<calculated when request is sent>', // This is usually dynamically generated and may not be needed.
-        'Content-Type': 'application/json',
-        'Content-Length': '<calculated when request is sent>', // Usually set automatically by axios.
-        Host: '<calculated when request is sent>', // Also set automatically.
-        'User-Agent': 'PostmanRuntime/7.41.2', // You can set this to something custom if needed.
-        Accept: '*/*',
-        'Accept-Encoding': 'gzip, deflate, br',
-        Connection: 'keep-alive',
-      },
-    },
-  );
+  try {
+    const agent = new https.Agent({
+      // Enforces using TLS 1.2 or higher
+      secureProtocol: 'TLSv1_2_method',
+    });
 
-  // Handle the response data if needed
-  return data;
+    const { data } = await axios.post(
+      'https://api.fortis-tele.com/api/SendSMS',
+      {
+        api_id: process.env.MESSAGE_PROVIDER_API_ID,
+        api_password: process.env.MESSAGE_PROVIDER_API_PASSWORD,
+        sms_type: 'T',
+        encoding: 'T',
+        sender_id: process.env.MESSAGE_PROVIDER_SENDER_ID,
+        phonenumber: phoneNumber,
+        textmessage: message,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'User-Agent': 'PostmanRuntime/7.41.2',
+          Accept: '*/*',
+          'Accept-Encoding': 'gzip, deflate, br',
+          Connection: 'keep-alive',
+        },
+        httpsAgent: agent,
+      },
+    );
+
+    return data;
+  } catch (error) {
+    console.error('Error sending message:', error);
+    throw error; // Re-throw the error after logging
+  }
 };

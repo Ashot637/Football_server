faafef9d3f954d0e613a2e93cfb04aa9219b0ab4
nikoghosian <vahe20006@gmail.com>
2024-09-05 14:34:56 +0400

    test feature

--- src/helpers/sendMessageToNumber.ts
+++ src/helpers/sendMessageToNumber.ts
@@ -1,40 +1,19 @@
-import axios from 'axios';
-import https from 'https';
+const axios = require('axios');
 
-// Create an HTTPS agent that enforces TLS 1.2 or higher
-const httpsAgent = new https.Agent({
-  secureProtocol: 'TLSv1_2_method', // Forces the use of TLS 1.2
-});
-
-// Your Axios request with the custom HTTPS agent
 export const sendMessageToNumber = async (phoneNumber: string, message: string) => {
   try {
-    const response = await axios.post(
-      'https://api.fortis-tele.com/api/SendSMS',
-      {
-        api_id: process.env.MESSAGE_PROVIDER_API_ID,
-        api_password: process.env.MESSAGE_PROVIDER_API_PASSWORD,
-        sms_type: 'T',
-        encoding: 'T',
-        sender_id: process.env.MESSAGE_PROVIDER_SENDER_ID,
-        phonenumber: phoneNumber,
-        textmessage: message,
-      },
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          Accept: '*/*',
-          'User-Agent': 'PostmanRuntime/7.41.2',
-          'Accept-Encoding': 'gzip, deflate, br',
-          Connection: 'keep-alive',
-        },
-        httpsAgent, // Use the custom agent
-      },
-    );
+    const response = await axios.post('https://api.fortis-tele.com/api/SendSms', {
+      api_id: 'API203452465',
+      api_password: 'y"EnaM!Z6L',
+      sms_type: 'P',
+      encoding: 'T',
+      sender_id: 'Ballhola',
+      phonenumber: '37495444083',
+      textmessage: 'test',
+    });
 
-    return response.data;
-  } catch (error) {
-    console.error('Error sending message:', error);
-    throw error; // Rethrow the error for further handling
+    console.log('Response:', response.data);
+  } catch (error: any) {
+    console.error('Error sending SMS:', error.response ? error.response.data : error.message);
   }
 };
